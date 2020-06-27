1e73cfeb2716aebc9634f93bcd2d0b181e18eb84
frankmagoba <franklinmagoba4@gmail.com>
2020-06-27 14:18:05 +0300

    add a custom middleware to the route.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = new express();
 
+const auth = () => {
+    return (req, res, next) => {
+        next()
+    }
+}
+
 app.post('/authenticate', (req, res) => {
     res.status(200).json({ "statusCode": 200, "message": "hello" });
 });
