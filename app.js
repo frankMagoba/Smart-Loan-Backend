01b9277201e10b44ee7bf151a609f85946554356
frankmagoba <franklinmagoba4@gmail.com>
2020-06-27 14:23:58 +0300

    hardcoded the username and password on local strategy

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = new express();
-const  passport  =  require('passport');
-const  LocalStrategy  =  require('passport-local').Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const auth = () => {
     return (req, res, next) => {
         next()
     }
 }
-
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        if (username === "admin" && password === "admin") {
+            return done(null, username);
+        } else {
+            return done("unauthorized access", false);
+        }
+    }
+));
 app.post('/authenticate', (req, res) => {
     res.status(200).json({ "statusCode": 200, "message": "hello" });
 });
