0d1a4816215f074fad3f10bed10998545e12e1dd
frankmagoba <franklinmagoba4@gmail.com>
2020-06-27 14:19:54 +0300

    Require both passport and passport-local in app.js..

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = new express();
+const  passport  =  require('passport');
+const  LocalStrategy  =  require('passport-local').Strategy;
 
 const auth = () => {
     return (req, res, next) => {
