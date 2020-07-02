8c8c82ff14acd9317ed84455b2b75ef030626997
frankmagoba <franklinmagoba4@gmail.com>
2020-07-02 13:51:17 +0300

    app name

--- users/users.controller.js
+++ users/users.controller.js
@@ -14,6 +14,7 @@ router.delete('/:id', _delete);
 module.exports = router;
 
 function authenticate(req, res, next) {
+    res.set('Access-Control-Allow-Credentials', 'true')
     userService.authenticate(req.body)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
         .catch(err => next(err));
